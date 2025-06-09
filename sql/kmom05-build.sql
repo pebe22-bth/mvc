0de716492620b45eaf153ccd3eba1a9ae392313c
Pelle Bengtsson <pebe22@student.bth.se>
2025-06-09 23:45:57 +0200

    Book class added

--- sql/kmom05-build.sql
+++
@@ -1,6 +0,0 @@
-# Create user and grant privileges
-
-create user 'pebe22'@'localhost' identified by 'geZkZcwDKRB2';
-create user 'pebe22' identified by 'geZkZcwDKRB2';
-grant all privileges on pebe22.* to 'pebe22'@'%';
-grant all privileges on pebe22.* to 'pebe22'@'localhost';
